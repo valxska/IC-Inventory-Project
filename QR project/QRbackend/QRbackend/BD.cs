39448bb8f09adc346abb389a282499e2cf2649c7
valxska <49490453+valxska@users.noreply.github.com>
2019-04-22 16:15:09 -0600

    New project in Visual Studio

---
+++ QR project/QRbackend/QRbackend/BD.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Funciones respectivas a la BD
+
+namespace QRbackend
+{
+    class BD
+    {
+        private MySqlConnection conection = new MySqlConnection("server = localhost; user id = root; password = Poder*16; persistsecurityinfo = True; database = inventario_ic");
+        
+    }
+}
