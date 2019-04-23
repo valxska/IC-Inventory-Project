5ad0ddea2fd5c0239966cffb192b44bbb2130704
valxska <49490453+valxska@users.noreply.github.com>
2019-04-22 22:44:48 -0600

    Se agrega git ignore, se hizo LogIn y Agregar Brand

--- QR project/QRbackend/QRbackend/BD.cs
+++ QR project/QRbackend/QRbackend/BD.cs
@@ -11,7 +11,92 @@ namespace QRbackend
 {
     class BD
     {
-        private MySqlConnection conection = new MySqlConnection("server = localhost; user id = root; password = Poder*16; persistsecurityinfo = True; database = inventario_ic");
-        
+        private MySqlConnection connection = new MySqlConnection("server = localhost; user id = root; password = Poder*16; persistsecurityinfo = True; database = inventario_ic");
+     
+     public bool LogIn (String pWWID, String pPassword)
+     {
+            this.connection.Open();
+
+
+            MySqlCommand code = new MySqlCommand();
+            code.Connection = this.connection;
+
+            code.CommandText = ("Select * from user Where WWID = '" + pWWID + "' and password = '" + pPassword + "' ");
+
+            MySqlDataReader rdr = code.ExecuteReader();
+         
+
+            if (rdr.Read())
+            {
+                this.connection.Close();
+                return true;
+            }
+            else
+            {
+                this.connection.Close();
+                return false;
+            }
+
+            
+
+
+        }
+        //Terminar
+        public void AddDevice(String pQR, String pSerialCode, String pPrice, String pDescription, String PBrand, String pEstate, String pCategory)
+        {
+            MySqlCommand code = new MySqlCommand();
+            code.Connection = this.connection;
+
+            //code.CommandText = ("Insert Into devices  (QR, serialCode, price, description, idBrand, available, idEstate) Values ('"+ pQR +"' , '"+ pSerialCode +"', "+ pPrice +", '"+ pDescription +"', "+ idBrand+", 1 , "+ idEstate);
+
+            MySqlDataReader rdr = code.ExecuteReader();
+
+
+            if (rdr.Read())
+            {
+                this.connection.Close();
+                //return true;
+            }
+            else
+            {
+                this.connection.Close();
+                //return false;
+            }
+        }
+
+        public bool AddBrand(String pBrandName)
+            {
+            this.connection.Open();
+
+
+            MySqlCommand code = new MySqlCommand();
+            code.Connection = this.connection;
+
+            code.CommandText = ("Select idBrand From brand where brandName = '"+ pBrandName +"' ");
+
+            MySqlDataReader rdr = code.ExecuteReader();
+
+
+            if (rdr.Read())
+            {
+                this.connection.Close();
+                return false;
+            }
+            else
+            {
+                this.connection.Close();
+                this.connection.Open();
+                code.Connection = this.connection;
+
+                code.CommandText = ("Insert Into brand (brandName) Values ('" + pBrandName + "') ");
+                code.ExecuteReader();
+                this.connection.Close();
+
+                return true;
+                
+            }
+
+        }
+
     }
 }
--- QR project/QRbackend/QRbackend/Program.cs
+++ QR project/QRbackend/QRbackend/Program.cs
@@ -14,9 +14,21 @@ namespace QRbackend
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            BD PruebaDeLogIn = new BD();
+            bool x = PruebaDeLogIn.AddBrand("Alienware");
+            if (x) {
+
+                MessageBox.Show("Funcionó");
+            }
+            else
+            {
+                MessageBox.Show("No funcionó");
+            }
+           
+
         }
     }
 }
