bf6328b8b38afd1030d13c55d30186526e878b58
Nakisha <nakisha.s.dixon.lawrence@intel.com>
2019-04-25 15:46:42 -0600

    Add Device
    Se añadio el form para añadir los devices

--- QR project/QRbackend/QRbackend/AddDevice.cs
+++ QR project/QRbackend/QRbackend/AddDevice.cs
@@ -16,5 +16,15 @@ namespace QRbackend
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- QR project/QRbackend/QRbackend/LoginForm.cs
+++ QR project/QRbackend/QRbackend/LoginForm.cs
@@ -16,5 +16,10 @@ namespace QRbackend
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
