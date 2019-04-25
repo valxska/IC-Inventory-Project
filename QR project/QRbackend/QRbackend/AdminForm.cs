21444443455ed9c01af6c8e8a614a2d67531cf0f
Nakisha <nakisha.s.dixon.lawrence@intel.com>
2019-04-25 14:45:59 -0600

    LoginForm, AdminForm, InternForm
    Se realizo sin funcionalidad los Forms de Administrador, de Interno y Log In

--- QR project/QRbackend/QRbackend/AdminForm.cs
+++ QR project/QRbackend/QRbackend/AdminForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QRbackend
 {
-    public partial class Form1 : Form
+    public partial class AdminForm : Form
     {
-        public Form1()
+        public AdminForm()
         {
             InitializeComponent();
         }
--- QR project/QRbackend/QRbackend/LoginForm.Designer.cs
+++ QR project/QRbackend/QRbackend/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QRbackend
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 244);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "LoginForm";
         }
 
         #endregion
     }
 }
-
