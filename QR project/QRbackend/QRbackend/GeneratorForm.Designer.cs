f4042adbb50826fb76cac7df83dbefd473c25348
Nakisha <nakisha.s.dixon.lawrence@intel.com>
2019-04-26 21:43:49 -0600

    Insert Device
    Algoritmo implementado. Falta pruebas, de insert device

--- QR project/QRbackend/QRbackend/GeneratorForm.Designer.cs
+++ QR project/QRbackend/QRbackend/GeneratorForm.Designer.cs
@@ -34,6 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.qrtext = new System.Windows.Forms.TextBox();
             this.backButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picturebox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,6 +55,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // generatebutton
             // 
@@ -94,11 +96,22 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(457, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Create";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // GeneratorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(725, 509);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.qrtext);
             this.Controls.Add(this.label1);
@@ -121,5 +134,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox qrtext;
         private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button button2;
     }
 }
