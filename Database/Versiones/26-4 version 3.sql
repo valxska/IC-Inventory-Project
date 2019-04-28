afb3f57f4c065d562f4157271f190dac0f193b03
valxska <49490453+valxska@users.noreply.github.com>
2019-04-28 17:04:02 -0600

    Data de prueba

--- Database/Versiones/26-4 version 3.sql
+++ Database/Versiones/26-4 version 3.sql
@@ -1,14 +1,16 @@
  Insert Into user (WWID, password) Values (11880674,1234);
  Select * from user;
  
- Insert Into devices  (QR, serialCode, price, description, idBrand, available, idEstado) Values ("cut_my_lip", 1234, 20000, "Sirve para llamar", 1, 1 , 1);
-	Insert Into brand (brandName) Values ("Samsung");
+ Insert Into devices  (QR, serialCode, price, description, idBrand, available, idEstado) Values ("Arduino", "GS75A9AV", 190000, "", 10, 1 , 1);
+	Insert Into brand (brandName) Values ("Arduino");
     Insert into category(categoryName) Values ("Virtual Reality");
-    Insert Into person (ID, personName, personLastName, idType, available, authorized) Values (1173923, "Rubio", "Escobar", 2, 1,0);
+    Insert Into person (ID, personName, personLastName, idType, available, authorized) Values (1682849, "Santiago", "Rojas", 2, 1,0);
     Insert Into estate (estateName) Values ("Bueno");
     Insert Into user (WWID, password, idPerson) Values (116440524, 1234,1);
 	Insert Into personaltype (nombre) Values ("Interno");
     Insert Into eventtype (name) Values ("Lend");
+   
+   
     select * From personaltype;
 	Select * From user;
     Select * From brand;
