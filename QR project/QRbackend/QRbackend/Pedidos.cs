9b23d658484dd339b115768ba6b2c7411ad6b0bc
Nakisha <nakisha.s.dixon.lawrence@intel.com>
2019-04-30 21:50:08 -0600

    Comments
    Comentarios de linea

--- QR project/QRbackend/QRbackend/Pedidos.cs
+++ QR project/QRbackend/QRbackend/Pedidos.cs
@@ -65,11 +65,13 @@ namespace QRbackend
 
 
                 int idPerson = bd.VerifyPerson(id, name, lastname, tipo + 1, allowed, email, phone);
+                //VerifyPerson retorna 0 si no encontro Id o el id si encontro uno
                 if (idPerson == 0)
                 {
                     String IDperson = bd.AddPerson(id, name, lastname, tipo, allowed, email, phone);
                 }
                 //int idPerson = bd.SearchPerson(id);
+                //En caso de que no este, entra al if y al salir ya esta insertado retornando el pId si se inserto, si no, retorna un ""
                 bd.AddEvent(bd.VerifyDevice(device), bd.VerifyPerson(id, name, lastname, tipo + 1, allowed, email, phone), idBorrowPerson, eventtype, description);
                 
 
